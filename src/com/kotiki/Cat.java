4b290ea49232030084231bd32d8b5f00d2fc1946
Andrey Strelets <43443151+E244AA@users.noreply.github.com>
2019-02-11 15:14:20 +0300

    Update Cat.java

--- src/com/kotiki/Cat.java
+++ src/com/kotiki/Cat.java
@@ -9,23 +9,38 @@ public class Cat {
     private int age;
     private int hp;
     private int hunger;
+    private int famaly;
+    private Cat spouse = null;
 
-    public Cat(String name, boolean male){
+    public Cat(String name, int famaly){
+        Random r = new Random();
+
+        int a = r.nextInt(1);
+        a == 0 ? this.male = true : this.male = false;
+        
         this.name = name;
-        this.male = male;
         this.age = 0;
         this.hunger = 100;
-
-        Random r = new Random();
+        this.famaly = famaly;
         this.hp = r.nextInt(100);
     }
 
     private boolean isAlive(){
         return hp > 0 ? true : false;
     }
-
     private boolean isHunger(){
         return hunger > 50 ? false : true;
+    }
+
+
+    private void marry(Cat spouse){
+        if(this.age >= 18) this.spouse = spouse;
+    }
+
+    private void giveBirth(){
+        if(spouse != null){
+            
+        }
     }
 
 }
